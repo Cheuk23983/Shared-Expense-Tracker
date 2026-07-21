7401d573ed18373342b4311c23b9c7c2d053c405
hello world <st23983@howick.school.nz>
2026-07-22 10:16:59 +1200

    Added class and the main window config

--- Group Expense Tracker/main_menu(launcher).py
+++ Group Expense Tracker/main_menu(launcher).py
@@ -6,4 +6,31 @@
 # import customtkinter module
 import customtkinter as ctk
 
+# Set global default appearance mode and efault color theme
+ctk.set_appearance_mode("System")
+ctk.set_default_color_theme("blue")
+
 # launcher class
+class MainMenuFrame:
+    '''Launcher window for the tracker'''
+    def __init__(self, root, select_trip, create_trip):
+        self.root = root
+        self.root.title("Group Expense Tracker")
+        self.root.resizeable(False,False)
+        self.select_trip = select_trip
+        self.create_trip = create_trip
+        
+        # config frame grid layout
+        self.root.grid_columnconfig(0,weight=1)
+        self.root.grid_rowconfig(2, weight=1)
+        
+        # make the GUI widgets
+        self.create_widgets()
+        
+
+
+        
+        
+        
+        
+        
