d7b25e983fec601cd505ba9dc434a0a972005485
hello world <st23983@howick.school.nz>
2026-07-21 15:18:37 +1200

    This version include documentation

---
+++ Group Expense Tracker/main_menu(launcher).py
@@ -0,0 +1,9 @@
+# Purpose: This program is the first component of the tracker project. The Component included function to manage trips. 
+# Author: Hubert Kwan
+# Date: 21/07/2026
+# Version: 1.0
+
+# import customtkinter module
+import customtkinter as ctk
+
+# launcher class
