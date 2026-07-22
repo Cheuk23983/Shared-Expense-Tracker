df5fced3da138c7ae36a27a50fed998f865c20fd
hello world <st23983@howick.school.nz>
2026-07-23 09:28:05 +1200

    Included trials for reading json files

--- Group Expense Tracker/main_menu(launcher).py
+++ Group Expense Tracker/main_menu(launcher).py
@@ -16,6 +16,8 @@ class MainMenuFrame:
     def __init__(self, root):
         self.root = root
         self.root.title("Group Expense Tracker")
+        self.geometry("450x550")
+        self.folder_path = "trips"
         
         # config frame grid layout
         self.root.grid_columnconfig(0,weight=1)
@@ -36,7 +38,7 @@ class MainMenuFrame:
         self.select_mode_btn - ctk.CTkButton(self.action_frame, text="Select", width=80)
         self.select__mode_btn.pack(side="left")
         # add trip button
-        self.new_trip_btn = ctk.CTkButton(self.action_frame, text="+ New Trip", command=self.create_trip)
+        self.new_trip_btn = ctk.CTkButton(self.action_frame, text="+ New Trip", fg_color="#0080FF", command=self.create_trip)
         self.new_trip_btn.pack(side="right")
         
         # Frame for saved trips
@@ -47,3 +49,39 @@ class MainMenuFrame:
         self.bottom_frame.grid(row=3, column=0, padx=20, pady=(5, 15), sticky="ew")
         
         self.theme_switch = ctk.CTkSwitch(self.bottom_frame, text="Light/Dark Mode", command=self.toggle_theme)
+        self.theme_switch.pack(side="left")
+
+        # Load data on startup
+        self.load_and_display_trips()
+        
+    
+    def create_trip_click(self):
+        print("Opening Creation Window...")
+    
+    
+    def open_trip_click(self):
+        print(f"Opening trip...")
+        
+    
+    def load_and_display_trip(self):
+        for widget in self.trip_ui_widgets:
+            widget.destroy()
+        self.trip_ui_widgets = []
+
+
+        # trial for read and load json files
+        # trial A
+        trip_files = ["queenstown_trip.json", "japan_holiday.json"]
+        
+        # trial B
+        import os
+        if not os.path.exists(self.folder_path):
+            os.makedirs(self.folder_path)
+            
+        trips_file = [
+            os.path.join(self.folder_path, file)
+            for file in os.listdir(self.folder_path)
+            if file.endswith(".json")
+        ]
+        
+        
