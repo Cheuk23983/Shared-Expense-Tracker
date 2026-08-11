44703779a0bc9f6f5fe01b74581b26e6fdfd6276
hello world <st23983@howick.school.nz>
2026-08-11 15:17:49 +1200

    This updates includes the completed program for component 4.
    testing are not done yet.

--- Group Expense Tracker/component_4_expense_window.py
+++ Group Expense Tracker/component_4_expense_window.py
@@ -21,6 +21,7 @@ class ExpenseFormWindow:
     def __init__(self, root, file_path="trip/japan.json"):
         '''initialse the form window, with loading trip data and the UI'''
         self.root = root
+        # self.top = ctk.CTkToplevel(root)
         self.root.title("Expense Management Window")
         self.root.geometry("400x650")
         # self.top.lift()
@@ -65,12 +66,151 @@ class ExpenseFormWindow:
         self.entry_expense_title = ctk.CTkEntry(self.root, placeholder_text="e.g. Dinner in Sushiro", justify="center")
         self.entry_expense_title.grid(row=2, column=0, columnspan=2, padx=20, pady=(0, 8), sticky="ew")
         # Amount label
-        self.lbl_amount = ctk.CTkLabel(self.root, text="Title", font=ctk.CTkFont(size=15, weight="bold"))
-        self.lbl_amount.grid(row=1, column=0, columnspan=2, padx=20, pady=(5, 2), stick="w")
+        self.lbl_amount = ctk.CTkLabel(self.root, text="Amount", font=ctk.CTkFont(size=15, weight="bold"))
+        self.lbl_amount.grid(row=3, column=0, columnspan=2, padx=20, pady=(5, 2), stick="w")
         # Amount entry box
-        self.entry_amount = ctk.CTkEntry(self.root, placeholder_text="e.g. Dinner in Sushiro", justify="center")
-        self.entry_amount.grid(row=2, column=0, columnspan=2, padx=20, pady=(0, 8), sticky="ew")
+        self.entry_amount = ctk.CTkEntry(self.root, placeholder_text="e.g. $50", justify="center")
+        self.entry_amount.grid(row=4, column=0, columnspan=2, padx=20, pady=(0, 8), sticky="ew")
+        
+        self.lbl_date = ctk.CTkLabel(self.root, text="Date", font=ctk.CTkFont(size=15, weight="bold"))
+        self.lbl_date.grid(row=5, column=0, padx=(20, 5), pady=(5,2), sticky="w")
 
+        self.entry_date = ctk.CTkEntry(self.root, placeholder_text="dd/mm/yyyy", justify="center")
+        self.entry_date.grid(row=6, column=0, padx=(5, 20), pady=(0, 8), sticky="ew")
+        
+        self.lbl_category = ctk.CTkLabel(self.root, text="Category", font=ctk.CTkFont(size=15, weight="bold"))
+        self.lbl_category.grid(row=5, column=1, padx=(5, 20), pady=(5, 2), sticky="w")
+        self.combo_category = ctk.CTkOptionMenu(self.root, values=["Food", "Transport", "Accommodation", "Activities", "Other"])
+        self.combo_category.grid(row=6, column=1, padx=(20, 5), pady=(0, 8), sticky="ew")
+        
+        self.lbl_payer = ctk.CTkLabel(self.root, text="Payer", font=ctk.CTkFont(size=15, weight="bold"))
+        self.lbl_payer.grid(row=7, column=0, columnspan=2, padx=20, pady=(5, 2), sticky="w")
+        
+        if len(self.members_list):
+            payer_options = self.members_list
+        else:
+            payer_options = ["No member was found."]
+        self.combo_payer = ctk.CTkOptionMenu(self.root, values=payer_options)
+        self.combo_payer.grid(row=8, column=0, columnspan=2, padx=20, pady=(0, 10), sticky="ew")
+        
+        self.lbl_assign = ctk.CTkLabel(self.root, text="Assign Shares(Equal split)", font=ctk.CTkFont(size=15, weight="bold"))
+        self.lbl_assign.grid(row=9, column=0, columnspan=2, padx=20, pady=(5, 2), sticky="w")
+        
+        self.member_scrcoll_box = ctk.CTkScrollableFrame(self.root, height=130)
+        self.member_scrcoll_box.grid(row=10, column=0, columnspan=2, padx=20, pady=(0, 15), sticky="nsew")
+        
+        if len(self.members_list) == 0:
+            lbl_no_member = ctk.CTkLabel(self.member_scrcoll_box, text="No member in trip file", text_color="grey")
+            lbl_no_member.pack(pady=10)
+        else:
+            for person_name in self.members_list:
+                row_frame = ctk.CTkFrame(self.member_scrcoll_box, fg_color="transparent")
+                row_frame.pack(fill="x", pady=3)
+                
+                chk_var = ctk.BooleanVar(value=True)
+                chk_box = ctk.CTkCheckBox(row_frame, text=person_name, variable=chk_var)
+                chk_box.pack(side="left", padx=5)
+                
+                self.checkbox_widget[person_name] = chk_var
+                
+            
+        self.btn_cancel = ctk.CTkButton(self.root, text="Cancel", fg_color="gray", hover="#555555", command=self.root.destroy)
+        self.btn_cancel.grid(row=11, column=0, padx=(20, 5), pady=(0, 15), sticky='ew')
+        self.btn_confirm = ctk.CTkButton(self.root, text="Confirm", fg_color="#0080ff", command=self.confirm_btn_on_click)
+        self.btn_confirm.grid(row=11, column=1, padx=(5, 20), pady=(0, 15), sticky="ew")
+            
+
+    def show_error(self, message):
+        '''methods to show error message'''
+        messagebox.showerror("Error", message, parent=self.root)
+        
+    
+    def validate_inputs(self):
+        '''validates form inputs before create=ing the expense dictionary'''
+        title = self.entry_expense_title.get()
+        amount = self.entry_amount.get()
+        date = self.entry_date.get()
+        
+        if title == "":
+            self.show_error("Expense title cannot be blank!")
+            return False
+        try:
+            clean_amount = amount.replace("$", "")
+            parsed_amount = float(clean_amount)
+            if parsed_amount <= 0:
+                self.show_error("Amount must be a positive number!")
+                return False
+        except ValueError:
+            self.show_error("Please enter a valid number for 'Amount'.")
+            return False
+        
+        if date == "":
+            self.show_error("Date cannot be blank!")
+            return False
+        try:
+            datetime.strptime(date, "%d, %m, %Y")
+        except ValueError:
+            self.show_error("Date must be in the format of dd/mm/yyyy.")
+            return False
+        return True
+    
+    
+    def confirm_btn_on_click(self):
+        '''process input data and append the nre epxense dictionary to the json file'''
+        if not self.validate_inputs():
+            return
+        
+        title = self.entry_expense_title.get().strip()
+        amount_value = float(self.entry_amount.get().replace("$", "").strip())
+        date_value = self.entry_date.get().strip()
+        category = self.combo_category.get()
+        payer = self.combo_payer.get()
+        
+        selected_members = []
+        for person_name, chk_var in self.checkbox_widget.item():
+            if chk_var.get() == True:
+                selected_members.append(person_name)
+        
+        split_share_amount = amount_value / len(selected_members)
+        
+        expense_dict = {
+            "title": title,
+            "description": title,
+            "amount": amount_value,
+            "date": date_value,
+            "category": category,
+            "payer": payer,
+            "split_between": selected_members,
+            "share_per_person": round(split_share_amount, 2)
+        }
+
+        if os.path.exists(self.file_path):
+            try:
+                if "expenses" not in self.trip_info_dict:
+                    self.trip_info_dict["expenses"] = []
+                self.trip_info_dict["expenses"].append(expense_dict)
+
+                with open(self.file_path, "w", encoding="utf-8") as file_data:
+                    json.dump(self.trip_info_dict, file_data, indent=4)
+            except FileNotFoundError:
+                self.show_error("Could not find the trip file to write changes!")
+                return
+            
+        messagebox.showinfo("Success", f"Expense '{title}' successfully added to trip!", parent=self.root)
+        self.root.destroy()
+        
+        
+root = ctk.CTk()
+app = ExpenseFormWindow(root)
+root.mainloop()
+
+                
+                
+                
+        
+            
+            
+        
 
 root = ctk.CTk()
 app = ExpenseFormWindow(root)
